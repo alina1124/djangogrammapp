00389a10c10d7a631a61f34a8231fa711d3a34c7
Alina Dubova <dubova.alina124@gmail.com>
2022-12-09 14:20:40 +0200

    add railway settings

--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
